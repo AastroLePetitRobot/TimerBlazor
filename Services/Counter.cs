c18566c66c65cc36611252ce85126ec957a4f376
Bonnet <Bonnet@DESKTOP-LK7DJNB>
2020-09-08 17:42:25 +0200

    TimerBlazor v1

---
+++ Services/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimerBlazor.Services
+{
+    public class Counter
+    {
+        public int nombre
+        {
+            get;
+
+            set;
+        }
+        public int pas { get; set; }
+
+        
+        public void Decrement()
+        {
+            nombre = this.nombre - this.pas;
+            NotifyStateChanged();
+            
+        }
+        public event Action OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+    }
+}
---
+++ Services/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerBlazor.Services
+{
+    public class Timer
+    {
+        /*public int interval { get; set; }
+        System.Timers.Timer timer1 = new System.Timers.Timer();
+        public void InitTimer()
+        {
+           
+                timer1.Interval = interval * 1000;
+                timer1.Elapsed += Timer1Tick;
+                timer1.AutoReset = false;
+                timer1.Start();
+        }
+    }*/
+
+    }
+}
+
